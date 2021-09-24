824f38539bdaa84c5d48ec494ef78c6bf11bcae6
AUROTECH-001280\Varun Muppidi <varun.muppidi@aurotechcorp.com>
2021-09-24 17:45:32 -0400

    code modified

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.use(bodyParser.json());
 app.get("/movie/videobin", async (req, res) => {
   let url = req.query.id;
   let urlVideobin = `https://videobin.co/embed-${url}.html`;
+  res.redirect(await scrapeDataM3U8(urlVideobin));
+});
+
+app.get("/movie/mp4", async (req, res) => {
+  let url = req.query.id;
+  let urlVideobin = `https://videobin.co/embed-${url}.html`;
   res.redirect(await scrapeData(urlVideobin));
 });
 
@@ -19,6 +25,7 @@ app.get("/series/videobin", async (req, res) => {
   let urlVideobin = `https://videobin.co/embed-${url}.html`;
   res.redirect(await scrapeData(urlVideobin));
 });
+
 
 app.get("/movie/youtube", async (req, res) => {
   let id = req.query.id;
@@ -70,7 +77,20 @@ async function scrapeData(url) {
     const data1 = cheerio.load($("script")._root[0].children[0].data);
     var str = data1("script:not([src])")[5].children[0].data;
     var match = str.match(/,"https:(.*)v.mp4/)[0].replace(',"',"");
-    console.log(match);
+    return match;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+async function scrapeDataM3U8(url) {
+  try {
+    const body = await axios.get(url);
+    const $ = cheerio.load(body, { xmlMode: true });
+    const data1 = cheerio.load($("script")._root[0].children[0].data);
+    var str = data1("script:not([src])")[5].children[0].data;
+    var match = str.match(/https(.*)master.m3u8/)[0];
     return match;
   } catch (err) {
     console.error(err);
